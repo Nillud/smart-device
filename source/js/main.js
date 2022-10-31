1ab89eaad6917652acae29c9a8164f46c7a5d732
Nillud <agadullin.linar@mail.ru>
2022-10-31 12:07:01 +0500

    Оживление js

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
+import IMask from 'imask';
 
 // ---------------------------------
 
@@ -7,8 +8,119 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Utils
   // ---------------------------------
+  const pageBody = document.querySelector('[data-body]');
+  const buttonRequest = document.querySelector('[data-request-button]');
+  const modalRequest = document.querySelector('[data-modal]');
+  const buttonClose = document.querySelectorAll('[data-close-modal]');
+  const scrollButton = document.querySelector('[data-scroll-button]');
+  const questionsBlock = document.querySelector('[data-questions]');
+  const buttonMore = document.querySelector('[data-button-more]');
+  const aboutDescription = document.querySelector('[data-description]');
+
+  const accordeonSections = document.querySelector('[data-accordeon-sections]');
+  const accordeonContacts = document.querySelector('[data-accordeon-contacts]');
+
+  const phoneInput = document.querySelector('[data-phone]');
+  const phoneInputModal = document.querySelector('[data-phone-modal]');
+  const maskOptions = {
+    mask: '+{7}(000)000 00 00',
+    lazy: false,
+  };
+
+  const phoneMask = new IMask(phoneInput, maskOptions);
+  const phoneMaskModal = new IMask(phoneInputModal, maskOptions);
+
+  phoneInput.addEventListener('load', () => {
+    phoneMask();
+  });
+
+  phoneInputModal.addEventListener('load', () => {
+    phoneMaskModal();
+  });
 
   iosVhFix();
+
+  buttonRequest.addEventListener('click', () => {
+    if (modalRequest) {
+      modalRequest.classList.add('is-active');
+
+      pageBody.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape') {
+          modalRequest.classList.remove('is-active');
+        }
+      });
+    }
+  });
+
+  buttonClose.forEach((button) => {
+    button.addEventListener('click', () => {
+      if (modalRequest) {
+        modalRequest.classList.remove('is-active');
+      }
+    });
+  });
+
+  scrollButton.addEventListener('click', () => {
+    if (scrollButton) {
+      questionsBlock.scrollIntoView({
+        block: 'nearest',
+        behavior: 'smooth',
+      });
+    }
+  });
+
+  buttonMore.addEventListener('click', () => {
+    if (buttonMore) {
+      if (aboutDescription.classList.contains('is-active')) {
+        aboutDescription.classList.remove('is-active');
+        buttonMore.textContent = 'Подробнее';
+      } else {
+        aboutDescription.classList.add('is-active');
+        buttonMore.textContent = 'Свернуть';
+      }
+    }
+  });
+
+  if (accordeonSections) {
+    accordeonSections.classList.remove('nojs');
+    accordeonSections.classList.add('inactive');
+
+    accordeonSections.addEventListener('click', () => {
+      if (accordeonContacts.classList.contains('is-active')) {
+        accordeonContacts.classList.remove('is-active');
+        accordeonContacts.classList.add('inactive');
+      }
+
+      if (accordeonSections.classList.contains('inactive')) {
+        accordeonSections.classList.remove('inactive');
+        accordeonSections.classList.add('is-active');
+      } else {
+        accordeonSections.classList.remove('is-active');
+        accordeonSections.classList.add('inactive');
+      }
+    });
+  }
+
+  if (accordeonContacts) {
+    accordeonContacts.classList.remove('nojs');
+    accordeonContacts.classList.add('inactive');
+
+    accordeonContacts.addEventListener('click', () => {
+      if (accordeonSections.classList.contains('is-active')) {
+        accordeonSections.classList.remove('is-active');
+        accordeonSections.classList.add('inactive');
+      }
+
+      if (accordeonContacts.classList.contains('inactive')) {
+        accordeonContacts.classList.remove('inactive');
+        accordeonContacts.classList.add('is-active');
+      } else {
+        accordeonContacts.classList.remove('is-active');
+        accordeonContacts.classList.add('inactive');
+      }
+    });
+  }
+
 
   // Modules
   // ---------------------------------
